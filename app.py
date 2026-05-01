328b5ace3702a69dcc33c8fb075a0dcbe488888b
GibranMahardika <85073970+GibranMahardika@users.noreply.github.com>
2026-05-01 18:17:35 +0700

    commit 4
    perubahan pada product_detail agar muncul popup ketika klik image nya
    
    terus di index apaya lupa euy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from flask import Flask, render_template, request, redirect, url_for
 import csv
-from flask import Flask, render_template, abort
+import datetime
 
 app = Flask(__name__)
 
@@ -79,6 +80,34 @@ def kategori_produk(nama_kategori):
     return render_template('kategori.html', 
                            products=products_in_category, 
                            kategori=nama_kategori)
+
+@app.route('/pesan/<nama_produk>')
+def proses_pesanan(nama_produk):
+    # 1. Kumpulin data "Intel" dari user yang ngeklik
+    waktu_klik = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    ip_user = request.remote_addr # Ambil IP Address pembeli
+    device_user = request.user_agent.string # Ambil info (Pake Chrome, Safari, Android, atau iPhone)
+
+    # 2. Catat datanya ke dalam file log_klik.csv (Otomatis dibikin kalau belum ada)
+    with open('log_klik.csv', mode='a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        # Tulis baris baru di excel tracker lu
+        writer.writerow([waktu_klik, nama_produk, ip_user, device_user])
+
+    # 3. Cari link WhatsApp asli dari barang yang diklik
+    link_wa = "https://wa.me/628123456789" # Link cadangan kalau error
+    try:
+        with open('produk.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['nama'] == nama_produk:
+                    link_wa = row['link_beli']
+                    break
+    except Exception as e:
+        pass
+
+    # 4. Lempar (Redirect) pembeli ke WhatsApp secara otomatis!
+    return redirect(link_wa)
 
 if __name__ == '__main__':
     app.run(debug=True)
